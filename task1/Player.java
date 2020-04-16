56fa618eac950913d33a1d876d77d4bf10507180
Ilya-Brod-sky-au <63790546+Ilya-Brod-sky-au@users.noreply.github.com>
2020-04-16 15:00:12 +0300

    Create Player.java

---
+++ task1/Player.java
@@ -0,0 +1,34 @@
+package Threads;
+
+import static java.lang.Thread.sleep;
+
+public class Player implements Runnable {
+    private int N = 10;
+    private int queue;
+
+    public Player() {
+        queue = 0;
+    }
+
+    public synchronized void turn() throws InterruptedException {
+        if (queue == 0) {
+            queue = 1;
+            System.out.println("Ping 1");
+        }
+        else if (queue == 1) {
+            queue = 2;
+            System.out.println("Pong 2");
+        };
+        sleep(100);
+    }
+
+    @Override
+    public void run() {
+        try {
+            for(int i = 0; i < N; ++i)
+                turn();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
